14cbed3c9445fb1267fca45a80ebc577e8fbdfe0
Marius Ortega <ortegasaurelmarius@gmail.com>
2024-02-10 18:06:54 +0100

    initial commit

---
+++ download_data.py
@@ -0,0 +1,11 @@
+import os
+import json
+from kaggle.api.kaggle_api_extended import KaggleApi
+api = KaggleApi()
+api.authenticate()
+dataset_name = "ferraz/cotatenis-sneakers"
+destination_dir = os.getcwd() + "/data"
+os.makedirs(destination_dir, exist_ok=True)
+api.dataset_download_files(dataset_name, path=destination_dir, unzip=True)
+
+print("Dataset downloaded successfully.")
