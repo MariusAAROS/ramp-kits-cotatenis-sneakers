b2c6bad3a6da469818696239ccd123b578ffe397
Louise Davy <davylouise@gmail.com>
2024-02-27 22:20:58 +0100

    temp commit : trying to make the fit function work

--- submissions/starting_kit/batch_classifier.py
+++ submissions/starting_kit/batch_classifier.py
@@ -1,42 +1,58 @@
 import sys
 import git
-import pandas as pd
-from PIL import Image
-import numpy as np
-from matplotlib import pyplot as plt
-from sklearn.model_selection import train_test_split
 import torch
-from torch.utils.data import DataLoader
-from torchvision.transforms import functional as F
 
 
-root_path = git.Repo('.', search_parent_directories=True).working_tree_dir
+root_path = git.Repo(".", search_parent_directories=True).working_tree_dir
 
 sys.path.append(root_path)
 
-from cotatenis_sneakers.sneaker_dataset import SneakerDataset
-from cotatenis_sneakers.sneaker_transforms import get_transform, UnNormalize
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 folder = "data/private/"
 
-class BatchClassifier():
+
+class BatchClassifier:
     def __init__(self):
         self.correct = 0
         self.total_correct = 0
         self.total = 0
         self.print_every = 10
-        self.build_model()
+        self.model = self.build_model()
 
     def build_model(self):
-        model = torch.hub.load("pytorch/vision", "resnet50", weights="IMAGENET1K_V2")
-        model.fc = torch.nn.Linear(2048, 3)
-        self.model_ = model.to(device)
+        model = torch.hub.load("pytorch/vision", "mobilenet_v2", pretrained=True)
+        for param in model.parameters():
+            param.requires_grad = False
+        model.classifier[1] = torch.nn.Linear(model.last_channel, 3)
 
-    def fit(self, X=None, y=None):
-        pass # pretrained model
+        for param in model.classifier.parameters():
+            param.requires_grad = True
+
+        model = model.to(device)
+        return model
+
+    def fit(self, gen_builder):
+        batch_size = 32
+        gen_train, gen_valid, nb_train, nb_valid = (
+            gen_builder.get_train_valid_generators(
+                batch_size=batch_size, valid_ratio=0.1
+            )
+        )
+
+        # for i, data in enumerate(gen_train):
+        #     inputs, labels = data
+        #     inputs, labels = inputs.to(device), labels.to(device)
+
+        #     self.optimizer.zero_grad()
+
+        #     outputs = self.model(inputs)
+        #     self.loss = self.criterion(outputs, labels)
+        #     self.loss.backward()
+        #     self.optimizer.step()
 
     def predict_proba(self, images):
+        self.model.eval()
         predictions = []
         with torch.no_grad():
             for _, img in enumerate(images):
@@ -44,4 +60,3 @@ class BatchClassifier():
                 outputs = self.model_(img)
                 predictions.append(outputs.squeeze().detach().cpu().numpy())
         return predictions
-    
